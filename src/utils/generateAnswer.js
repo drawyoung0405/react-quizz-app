cbd3a548f4e4c066271e1448f158d3b56ddb3ca1
Vo Nhu Hoa <hoavn0934813785@gmail.com>
2025-05-25 14:37:42 +0700

    Generate Answer Function

--- src/utils/generateAnswer.js
+++ src/utils/generateAnswer.js
@@ -1,8 +1,10 @@
-export function generateAnswer(dataSource = [], index = 0) {
+export function generateAnswer(dataSource = [], index) {
     if (dataSource.length === 0) {
         return [];
     }
+    
     const question = dataSource[index];
+    console.log('question', question);
     const answer = [...question.incorrect_answers];
     answer.splice(Math.floor(Math.random() * 4), 0, question.correct_answer);
     return answer
